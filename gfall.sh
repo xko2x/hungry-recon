d75427764e0a235a22cba5a20c6efb1cbe42a58a
xko2x <69871589+xko2x@users.noreply.github.com>
2021-10-28 15:56:30 +0200

    Update gfall.sh

--- gfall.sh
+++ gfall.sh
@@ -7,19 +7,20 @@
 # - Gf-Patterns (https://github.com/1ndianl33t/Gf-Patterns) - Read the README.md for how to copy json file to ~/.gf/
 
 [ ! -f ~/gfpatterns ] && mkdir ~/gfpatterns  2&>1
-[ ! -f ~/gfpatterns/temp ] && mkdir ~/gfpatterns/temp/  2&>1
+[ ! -f ~/gfpatterns/$1 ] && mkdir ~/gfpatterns/$1  2&>1
+[ ! -f ~/gfpatterns/$1/temp ] && mkdir ~/gfpatterns/$1/temp/  2&>1
 
 
 cd ~/.gf
-ls *.json > ~/gfpatterns/temp/patterns
-cat ~/gfpatterns/temp/patterns | cut -d "." -f 1 > ~/gfpatterns/temp/gf-patterns
+ls *.json > ~/gfpatterns/$1/temp/patterns
+cat ~/gfpatterns/$1/temp/patterns | cut -d "." -f 1 > ~/gfpatterns/$1/temp/gf-patterns
 
-cd ~/gfpatterns/temp/
+cd ~/gfpatterns/$1/temp/
 echo $1 | gau | sort -u >> $1-waybackdata.txt     # if you have subdomains list, can use "cat subdomains.txt | gau | sort -u >> waybackdata"
-for patterns in $(cat ~/gfpatterns/temp/gf-patterns);
+for patterns in $(cat ~/gfpatterns/$1/temp/gf-patterns);
 do
-  cat $1-waybackdata.txt | gf $patterns | tee -a ~/gfpatterns/$1-$patterns.txt;
+  cat $1-waybackdata.txt | gf $patterns | tee -a ~/gfpatterns/$1/$1-$patterns.txt;
 done
-cd ~/gfpatterns
+cd ~/gfpatterns/$1
 find . -iname "*.txt" -size 0 -delete
-rm ~/gfpatterns/temp/patterns | rm  ~/gfpatterns/temp/gf-patterns
+rm ~/gfpatterns/$1/temp/patterns | rm  ~/gfpatterns/$1/temp/gf-patterns
